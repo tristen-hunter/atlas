528addcd24ba1ed6581b5115c7872d73cfbfbc16
tristen-hunter <tristenhunter0702@gmail.com>
2026-09-08 11:06:24 +0200

    initial commit

---
+++ landing-page/src/App.tsx
@@ -0,0 +1,20 @@
+import Demo from './components/Demo/Demo';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar'
+import SneakPeek from './components/SneakPeak/SneakPeek';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <SneakPeek />
+      <Demo />
+      <Footer />
+    </>
+  );
+}
+
+
+export default App
